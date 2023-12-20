9d0d9eaab3f93231deefddd9bb3e0a720d880ad9
huaimogu <1145737342@qq.com>
2023-12-20 13:43:58 +0800

    add crown info function,support 15.21

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,11 +12,21 @@
 #include "ghidra_export.h"
 #include "util.h"
 
+struct Monster{
+	int Id;
+	std::string Name;
+	float Capture;
+	float Mini;
+	float Silver;
+	float Gold;
+};
 
 static std::queue<std::pair<float, std::string>> messages;
 static std::map<void*, std::queue<std::pair<float, std::string>>> monsterMessages;
+static std::map<void*, bool> monsterChecked;
 static std::mutex lock;
-
+static struct Monster monsters[102];
+static bool isInit=false;
 
 using namespace loader;
 
@@ -24,32 +34,73 @@ void showMessage(std::string message) {
 	MH::Chat::ShowGameMessage(*(undefined**)MH::Chat::MainPtr, (undefined*) &message[0], -1, -1, 0);
 }
 
-void handleMonsterCreated(undefined* monster)
+void handleMonsterCreated(int id, undefined* monster)
 {
 	char* monsterPath = offsetPtr<char>(monster, 0x7741);
 	if (monsterPath[2] == '0' || monsterPath[2] == '1') {
 		LOG(INFO) << "Setting up health messages for " << monsterPath;
 		std::unique_lock l(lock);
 		monsterMessages[monster] = messages;
+		if (monsters[id].Capture != 0) {
+			monsterMessages[monster].push({ monsters[id].Capture, "<STYL MOJI_RED_DEFAULT>" + monsters[id].Name+"可以捕获了" + "</STYL>" });
+		}
+		isInit = false;
+		monsterChecked[monster] = false;
 	}
 }
 
 void checkHealth(void* monster) {
+	int monsterId = *offsetPtr<int>(monster, 0x12280);
 	void* healthMgr = *offsetPtr<void*>(monster, 0x7670);
 	float health = *offsetPtr<float>(healthMgr, 0x64);
 	float maxHealth = *offsetPtr<float>(healthMgr, 0x60);
+
+	if (!(health == 100 && maxHealth == 100)) {
+		isInit = true;
+	}
 
 	char* monsterPath = offsetPtr<char>(monster, 0x7741);
 	auto& monsterQueue = monsterMessages[monster];
 	std::string lastMessage;
 	while (!monsterQueue.empty() && health / maxHealth < monsterQueue.front().first) {
-		lastMessage = monsterQueue.front().second;
-		monsterQueue.pop();
-	}
-	if (!lastMessage.empty()) {
+		lastMessage = "<STYL MOJI_YELLOW_DEFAULT>" + monsters[monsterId].Name + monsterQueue.front().second + "</STYL>";
 		LOG(INFO) << "Message: " << lastMessage;
 		showMessage(lastMessage);
+		monsterQueue.pop();
 	}
+}
+
+void checkMonsterSize(void* monster) {
+	int monsterId = *offsetPtr<int>(monster, 0x12280);
+	float sizeModifier = *offsetPtr<float>(monster, 0x7730);
+	float sizeMultiplier = *offsetPtr<float>(monster, 0x188);
+	if (sizeModifier <= 0 || sizeModifier >= 2) {
+		sizeModifier = 1;
+	}
+	float monsterSizeMultiplier = sizeMultiplier / sizeModifier;
+
+	LOG(INFO) << "monster id: " << monsterId << ", size: " << monsterSizeMultiplier;
+
+	struct Monster Monster = monsters[monsterId];
+
+	std::string size;
+	if (monsterSizeMultiplier >= Monster.Gold) {
+		size = "<STYL MOJI_RED_DEFAULT>大金</STYL>";
+	}
+	else if (monsterSizeMultiplier >= Monster.Silver) {
+		size = "<STYL MOJI_YELLOW_DEFAULT>大银</STYL>";
+	}
+	else if (monsterSizeMultiplier <= Monster.Mini) {
+		size = "<STYL MOJI_RED_DEFAULT>小金</STYL>";
+	}
+	else {
+		size = "普通大小";
+	}
+
+	std::stringstream ss;
+	ss << Monster.Name << "尺寸是" << size;
+	std::string msg = ss.str();
+	showMessage(msg);
 }
 
 CreateHook(MH::Monster::ctor, ConstructMonster, void*, void* this_ptr, unsigned int monster_id, unsigned int variant)
@@ -60,34 +111,45 @@ CreateHook(MH::Monster::ctor, ConstructMonster, void*, void* this_ptr, unsigned 
 
 __declspec(dllexport) extern bool Load()
 {
-	if (std::string(GameVersion) != "421631") {
+	if (std::string(GameVersion) != "421652") {
 		LOG(ERR) << "Health Notes : Wrong version";
 		return false;
 	}
 
 	std::ifstream file("nativePC/plugins/HealthNotes.json");
 	if (file.fail()) {
-		LOG(ERR) << "Health notes : Config file not found";
+		LOG(ERR) << "Health notes : Monster data file not found";
 		return false;
 	}
 
 	LOG(INFO) << "Health notes loading";
-
+	
 	nlohmann::json ConfigFile = nlohmann::json::object();
 	file >> ConfigFile;
 
-	for (auto obj : ConfigFile["messages"])
+	for (auto ratio : ConfigFile["HealthRatio"])
 	{
-		messages.push({ obj["ratio"], obj["msg"] });
+		messages.push({ ratio, "血量还剩"+ std::to_string(int(double(ratio) * 100)) + "%" });
 	}
-
+	int index = 0;
+	for (auto obj : ConfigFile["Monsters"])
+	{
+		index = obj["Id"];
+		monsters[index].Id = obj["Id"];
+		monsters[index].Name = obj["Name"];
+		monsters[index].Capture = obj["Capture"];
+		monsters[index].Mini = obj["Mini"];
+		monsters[index].Silver = obj["Silver"];
+		monsters[index].Gold = obj["Gold"];
+		index++;
+	}
 
 	MH_Initialize();
 
 	HookLambda(MH::Monster::ctor,
 		[](auto monster, auto id, auto subId) {
 			auto ret = original(monster, id, subId);
-			handleMonsterCreated(monster);
+			handleMonsterCreated(id, monster);
 			return ret;
 		});
 	HookLambda(MH::Monster::dtor,
@@ -96,6 +158,7 @@ __declspec(dllexport) extern bool Load()
 			{
 				std::unique_lock l(lock);
 				monsterMessages.erase(monster);
+				monsterChecked.erase(monster);
 			}
 			return original(monster);
 		});
@@ -107,6 +170,16 @@ __declspec(dllexport) extern bool Load()
 				std::unique_lock l(lock);
 				for (auto [monster, queue] : monsterMessages) {
 					checkHealth(monster);
+				}
+				if (isInit) {
+					for (auto [monster, isChecked] : monsterChecked) {
+						if (!isChecked) {
+							checkMonsterSize(monster);
+							monsterChecked[monster] = true;
+						}
+						
+					}
+					
 				}
 			}
 		}
