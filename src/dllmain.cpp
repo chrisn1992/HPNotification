f34bd7424704153b045683d1db4a8b6ec9c11f42
sl3dge78 <gcollin78@gmail.com>
2024-10-13 22:09:52 +0200

    fix v15.22

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,18 +1,14 @@
 // dllmain.cpp : Définit le point d'entrée de l'application DLL.
-#include <fstream>
-#include <queue>
-#include <functional>
-#include <mutex>
-#include <algorithm>
-#include <filesystem>
-
-#include <windows.h>
-
-#include "minhook/MinHook.h"
-#include "json/json.hpp"
+#include "MinHook.h"
+#include "json.hpp"
 #include "loader.h"
-#include "ghidra_export.h"
-#include "util.h"
+#include "plugin-utils.h"
+#include "signatures.h"
+#include <fstream>
+#include <mutex>
+#include <queue>
+#include <windows.h>
+#include <filesystem>
 
 struct Monster{
 	int Id;
@@ -23,8 +19,9 @@ struct Monster{
 	float Gold;
 };
 
+
 static std::queue<std::pair<float, std::string>> messages;
-static std::map<void*, std::queue<std::pair<float, std::string>>> monsterMessages;
+static std::map<void *, std::queue<std::pair<float, std::string>>> monsterMessages;
 static std::map<void*, bool> monsterChecked;
 static std::mutex lock;
 static struct Monster monsters[102];
@@ -34,13 +31,19 @@ static bool isInit=false;
 static bool displayCapture = true;
 
 using namespace loader;
+using namespace plugin;
 
-void showMessage(std::string message) {
-	MH::Chat::ShowGameMessage(*(undefined**)MH::Chat::MainPtr, (undefined*) &message[0], -1, -1, 0);
-}
+std::string_view plugin::module_name = "HealthNotes";
 
-void handleMonsterCreated(int id, undefined* monster)
-{
+const int UENEMY_LEA_VFT_OFFSET = 0x43;
+const int CHAT_INSTANCE_OFFSET = 0x4d;
+
+static void *chat_instance = nullptr;
+static void (*show_message)(void *, const char *, float, unsigned int, char) = nullptr;
+
+void showMessage(std::string message) { show_message(*(void **)chat_instance, message.c_str(), -1, -1, 0); }
+
+void handleMonsterCreated(int id, void *monster) {
 	char* monsterPath = offsetPtr<char>(monster, 0x7741);
 	if (monsterPath[2] == '0' || monsterPath[2] == '1') {
 		LOG(INFO) << "Setting up health messages for " << monsterPath;
@@ -64,30 +67,27 @@ void handleMonsterCreated(int id, undefined* monster)
 	}
 }
 
-void checkHealth(void* monster) {
+void checkHealth(void *monster) {
 	int monsterId = *offsetPtr<int>(monster, 0x12280);
-	void* healthMgr = *offsetPtr<void*>(monster, 0x7670);
-	float health = *offsetPtr<float>(healthMgr, 0x64);
-	float maxHealth = *offsetPtr<float>(healthMgr, 0x60);
+  void *healthMgr = *offsetPtr<void *>(monster, 0x7670);
+  float health = *offsetPtr<float>(healthMgr, 0x64);
+  float maxHealth = *offsetPtr<float>(healthMgr, 0x60);
 
 	if (!(health == 100 && maxHealth == 100)) {
 		isInit = true;
 	}
 
-	char* monsterPath = offsetPtr<char>(monster, 0x7741);
-	auto& monsterQueue = monsterMessages[monster];
-	std::string lastMessage;
-	while (!monsterQueue.empty() && health / maxHealth < monsterQueue.front().first) {
-		lastMessage = monsterQueue.front().second;
-		std::string re = "monstername";
-		while (lastMessage.find(re) != std::string::npos) {
-			size_t pos = lastMessage.find(re);
-			lastMessage.replace(pos, re.length(), monsters[monsterId].Name);
-		}
-		LOG(INFO) << "Message: " << lastMessage;
-		showMessage(lastMessage);
-		monsterQueue.pop();
-	}
+  char *monsterPath = offsetPtr<char>(monster, 0x7741);
+  auto &monsterQueue = monsterMessages[monster];
+  std::string lastMessage;
+  while (!monsterQueue.empty() && health / maxHealth < monsterQueue.front().first) {
+    lastMessage = monsterQueue.front().second;
+    monsterQueue.pop();
+  }
+  if (!lastMessage.empty()) {
+    log(INFO, "Message: {}", lastMessage);
+    showMessage(lastMessage);
+  }
 }
 
 void checkMonsterSize(void* monster) {
@@ -126,16 +126,14 @@ void checkMonsterSize(void* monster) {
 	showMessage(size);
 }
 
-CreateHook(MH::Monster::ctor, ConstructMonster, void*, void* this_ptr, unsigned int monster_id, unsigned int variant)
-{
-	LOG(INFO) << "Creating Monster : " << monster_id << "-" << variant << " @0x" << this_ptr;
-	return original(this_ptr, monster_id, variant);
+byte *get_lea_addr(byte *addr) {
+  auto base_addr = addr + 7; // size of call instruction
+  auto offset = *reinterpret_cast<int *>(addr + 3);
+  return base_addr + offset;
 }
 
-__declspec(dllexport) extern bool Load()
-{
+__declspec(dllexport) extern bool Load() {
 	std::string nativePCPath = "nativePC";
-
 	// check if using ICE mod
 	if (std::filesystem::exists("ICE")) {
 		if (std::filesystem::is_directory("ICE")) {
@@ -156,15 +154,14 @@ __declspec(dllexport) extern bool Load()
 	}*/
 
 	std::ifstream file(nativePCPath.append("/plugins/HealthNotes.json"));
-	if (file.fail()) {
-		LOG(ERR) << "Health notes : Monster data file not found";
-		return false;
-	}
+  if (file.fail()) {
+    log(ERR, "Error: config file not found");
+    return false;
+  }
 
-	LOG(INFO) << "Health notes loading";
-	
-	nlohmann::json ConfigFile = nlohmann::json::object();
-	file >> ConfigFile;
+  LOG(INFO) << "Health notes loading";
+  nlohmann::json ConfigFile = nlohmann::json::object();
+  file >> ConfigFile;
 
 	language = ConfigFile["Language"];
 	displayCapture = ConfigFile["DisplayCapture"];
@@ -198,33 +195,42 @@ __declspec(dllexport) extern bool Load()
 		index++;
 	}
 
-	MH_Initialize();
+  auto uenemy_ctor_addr = find_func(sig::monster_ctor);
+  auto uenemy_reset_addr = find_func(sig::monster_reset);
+  auto show_message_addr = find_func(sig::show_message);
+  auto message_instance_addr = find_func(sig::chat_instance_source);
+  if (!uenemy_ctor_addr || !show_message_addr || !message_instance_addr || !uenemy_reset_addr)
+    return false;
 
-	HookLambda(MH::Monster::ctor,
-		[](auto monster, auto id, auto subId) {
-			auto ret = original(monster, id, subId);
-			handleMonsterCreated(id, monster);
-			return ret;
-		});
-	HookLambda(MH::Monster::dtor,
-		[](auto monster) {
-			LOG(INFO) << "Monster destroyed " << (void*)monster;
-			{
-				std::unique_lock l(lock);
-				monsterMessages.erase(monster);
-				monsterChecked.erase(monster);
-			}
-			return original(monster);
-		});
+  show_message = reinterpret_cast<decltype(show_message)>(*show_message_addr);
+  chat_instance = get_lea_addr(*message_instance_addr + CHAT_INSTANCE_OFFSET);
+  log(INFO, "chat_instance:{:p}", chat_instance);
 
-	std::thread([]() {
-		while (true) {
-			std::this_thread::sleep_for(std::chrono::seconds(2));
-			{
-				std::unique_lock l(lock);
-				for (auto [monster, queue] : monsterMessages) {
-					checkHealth(monster);
-				}
+  MH_Initialize();
+
+  Hook<void *(void *, int, int)>::hook(*uenemy_ctor_addr, [](auto orig, auto this_, auto id, auto subid) {
+    auto ret = orig(this_, id, subid);
+    handleMonsterCreated(id, this_);
+    return ret;
+  });
+  Hook<void *(void *)>::hook(*uenemy_reset_addr, [](auto orig, auto this_) {
+    {
+      std::unique_lock l(lock);
+      if (monsterMessages.erase(this_)) {
+        log(INFO, "Monster {:p} removed", this_);
+      }
+    }
+    return orig(this_);
+  });
+
+  std::thread([]() {
+    while (true) {
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+      {
+        std::unique_lock l(lock);
+        for (auto [monster, queue] : monsterMessages) {
+          checkHealth(monster);
+        }
 				if (isInit) {
 					for (auto [monster, isChecked] : monsterChecked) {
 						if (!isChecked) {
@@ -235,22 +241,17 @@ __declspec(dllexport) extern bool Load()
 					}
 					
 				}
-			}
-		}
-	}).detach();
+      }
+    }
+  }).detach();
 
-	MH_ApplyQueued();
+  MH_ApplyQueued();
 
-	return true;
+  return true;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
-	LPVOID lpReserved
-)
-{
-	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-		return Load();
-    return TRUE;
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+  if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    return Load();
+  return TRUE;
 }
-
